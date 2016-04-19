95dcf6325e41556cafa216406e42dd019a4cb9a7
Dylan Waij <d_way@live.nl>
2016-04-19 09:05:46 +0200

    PauseMenu Push

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class PauseMenu : MonoBehaviour {
+
+    bool Esc;
+    [SerializeField] GameObject CheckCanvas; [SerializeField] GameObject panel;
+
+    void Update()
+    {
+        CheckIfPaused();
+    }
+    void CheckIfPaused()
+    {
+        if (Esc = Input.GetKey(KeyCode.Escape))
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+    public void TogglePause()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1.0f;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
