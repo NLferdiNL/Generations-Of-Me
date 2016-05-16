67bb6649329ef2bb584a978205a3281683377fbc
Swan Chase <swancd@gmail.com>
2016-05-17 00:00:46 +0200

    small changes to level 2

---
+++ Assets/_Scripts/Spikes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour
+{
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            other.GetComponent<PlayerMovement>();
+        }
+    }
+
+}
