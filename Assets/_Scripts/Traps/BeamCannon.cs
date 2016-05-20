c99636f2d72472cf98f4d645cee47c4e7e7d1255
Swan Chase <swancd@gmail.com>
2016-05-20 12:09:39 +0200

    ima FIRIN MA LAZOR
    BHUEEUEUEUUEUE

---
+++ Assets/_Scripts/Traps/BeamCannon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamCannon : MonoBehaviour
+{
+    Respawner reps;
+
+    void Start()
+    {
+        reps = Camera.main.GetComponent<Respawner>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            reps.Respawn(other.gameObject);
+        }
+    }
+}
