46ace941627416b6fff21f367707e6b7a31e65fc
Dylan Waij <d_way@live.nl>
2016-05-26 09:24:40 +0200

    MenuPush

---
+++ Assets/_Scripts/UI/MenuHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class MenuHandler : MonoBehaviour {
+
+    public string loadLevel;
+
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
